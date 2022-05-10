b9d71465268489ba18da0b1d9b9769af8fd682e8
gcnw <SkillCatcher@pm.me>
2022-05-10 12:40:32 -0700

    Desktop version mostly done. JS with else-click closes menus.

--- script.js
+++ script.js
@@ -3,23 +3,34 @@ const companyButton = document.getElementById('companyButton');
 
 function changeArrow(selection){
     const inputNode = selection.currentTarget;
-    Array.from(document.getElementsByClassName('arrowImage')).forEach(element => element.src = './images/icon-arrow-down.svg');
-    Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.display = 'none');
     const target = inputNode.parentNode.getElementsByTagName('div')[0];
     let currentArrow = inputNode.getElementsByTagName('img')[0].src;
+    /* SET ALL MENUS TO CLOSE AND ARROWS TO DOWN*/
+    Array.from(document.getElementsByClassName('arrowImage')).forEach(element => element.src = './images/icon-arrow-down.svg');
+    Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.display = 'none');
+
+    /* OPEN SELECTED MENU */
     if(currentArrow.includes('arrow-down')){   
-        console.log('arrow-down');
-        Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.display = 'none');
         inputNode.getElementsByTagName('img')[0].src = './images/icon-arrow-up.svg';
         target.style.display = 'flex';
     }
+    /* CLOSE SELECTED MENU */
     else if (currentArrow.includes('arrow-up')){
-        console.log('arrow-up');
         inputNode.getElementsByTagName('img')[0].src = './images/icon-arrow-down.svg';
         target.style.display = 'none';
     }
 }
 
+/* DETECT CLICK LOCATION AND RESET MENUS IF CLICK IS NOT ON A MENU*/
+function bodyClick(selection){
+    selection.preventDefault();
+    if(!featuresButton.contains(selection.target) && !companyButton.contains(selection.target)){
+    Array.from(document.getElementsByClassName('arrowImage')).forEach(element => element.src = './images/icon-arrow-down.svg');
+    Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.display = 'none');  
+    }
+}
+
 featuresButton.addEventListener('click',changeArrow);
-companyButton.addEventListener('click',changeArrow); 
+companyButton.addEventListener('click',changeArrow);
+document.body.parentNode.addEventListener('click', bodyClick);
 
