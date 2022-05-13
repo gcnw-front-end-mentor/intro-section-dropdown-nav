f288e1e45ecf81835998cf38c90b568317374cf1
gcnw <SkillCatcher@pm.me>
2022-05-12 17:27:09 -0700

    mobile is looking better. sidenav still tough :|

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const featuresButton = document.getElementById('featuresButton');
 const companyButton = document.getElementById('companyButton');
+Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.display = 'none');
+Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.height = '0');
 
 function changeArrow(selection){
     const inputNode = selection.currentTarget;
@@ -8,16 +10,20 @@ function changeArrow(selection){
     /* SET ALL MENUS TO CLOSE AND ARROWS TO DOWN*/
     Array.from(document.getElementsByClassName('arrowImage')).forEach(element => element.src = './images/icon-arrow-down.svg');
     Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.display = 'none');
+    Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.height = '0px');
 
     /* OPEN SELECTED MENU */
     if(currentArrow.includes('arrow-down')){   
         inputNode.getElementsByTagName('img')[0].src = './images/icon-arrow-up.svg';
-        target.style.display = 'flex';
+        Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.display = 'flex');
+        Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.height = '')
     }
     /* CLOSE SELECTED MENU */
     else if (currentArrow.includes('arrow-up')){
         inputNode.getElementsByTagName('img')[0].src = './images/icon-arrow-down.svg';
-        target.style.display = 'none';
+        Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.display = 'none');
+        Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.height = '0px')
+
     }
 }
 
@@ -26,8 +32,15 @@ function bodyClick(selection){
     selection.preventDefault();
     if(!featuresButton.contains(selection.target) && !companyButton.contains(selection.target)){
     Array.from(document.getElementsByClassName('arrowImage')).forEach(element => element.src = './images/icon-arrow-down.svg');
-    Array.from(document.getElementsByClassName('menuContent')).forEach(element => element.style.display = 'none');  
     }
+}
+
+function openNav() {
+    document.getElementById('linkContainer').style.width = '240px';
+}
+
+function closeNav() {
+    document.getElementById('linkContainer').style.width = '0px';
 }
 
 featuresButton.addEventListener('click',changeArrow);
